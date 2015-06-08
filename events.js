138ae907818cbd8bc7068607e65b19def7bed6b0
serializedowen <wjh199455@gmail.com>
2015-06-08 02:20:19 -0400

    First commit start page layout done

---
+++ events.js
@@ -0,0 +1,10 @@
+
+function checkLevel()
+{
+	if (!document.getElementById("level1").checked) && (!document.getElementById("level1").checked){
+		alert("Please select a level to proceed.");
+		return false;
+	} else {
+		alert("a");
+	}
+}
